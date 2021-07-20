304878feb85440b10c78d359bdbe818ed1d81ac9
Alcoholicorn <alcoholicorn@gmail.com>
2021-07-20 17:00:24 -0400

    updated to express

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const http = require('http');
+const express = require('express');
+// const routes = require('./routes');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware!');
+    res.send("<h1>ADD PRODUCT</h1>")
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>')
+})
+
+app.listen(3000);
