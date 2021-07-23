c2eeb1b9ce9b24487c587eae0e0f6e2069f02c8c
Alcoholicorn <alcoholicorn@gmail.com>
2021-07-23 13:17:02 -0400

    added basic json storage for products

--- routes/admin.js
+++ routes/admin.js
@@ -2,20 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+// const rootDir = require('../util/path');
+const productsController = require('../controllers/products')
 
 const router = express.Router();
 
-const products = [];
 
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-});
 
-router.post('/add-product',(req, res, next) => {
-    products.push({title: req.body.title})
-    res.redirect('/');
-});
+router.get('/add-product', productsController.getAddProduct);
 
-exports.routes = router;
-exports.products = products;
+router.post('/add-product', productsController.postAddProduct);
+
+module.exports = router;
